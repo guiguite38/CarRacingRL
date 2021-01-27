a32f33375aa11b223b97919b67fa6ebc5f707a53
Guillaume Grosse <45588739+guiguite38@users.noreply.github.com>
2021-01-27 15:46:32 +0100

    clean up project

--- main.py
+++ main.py
@@ -1,145 +1,173 @@
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPooling2D
+
+from CarRacingDQNAgent import CarRacingDQNAgent
+import cv2
 import numpy as np
-import gym  #requires OpenAI gym installed
-from tensorflow.python.framework import ops
-from tensorflow.keras import layers, Input, Model
-ops.reset_default_graph()
+import gym
+from collections import deque
 
-env = gym.envs.make("CarRacing-v0") 
+# Environnement
+ENV = gym.envs.make("CarRacing-v0")
 
-# action_space
-# Entre -1.0 et 1.0 en tuple de dimension 3
-# (steer, gas, brake)
+# Discretisation
+ACTION_SPACE    = [
+    (-1, 1, 0.2), (0, 1, 0.2), (1, 1, 0.2), # Plein gaz + 20% frein
+    (-1, 1,   0), (0, 1,   0), (1, 1,   0), # Plein gaz + 0% frein
+    (-1, 0, 0.2), (0, 0, 0.2), (1, 0, 0.2), # 0% gaz + 20% frein
+    (-1, 0,   0), (0, 0,   0), (1, 0,   0)  # 0% gaz + 0% frein
+],
 
-# observation_space
-# L'image : 96x96x3 entre 0 et 255
 
-input_dims = 2
-state_placeholder = Input(tf.float32, [None, input_dims]) 
+# input is 96*96*3 ints between 0 and 255
+# output must be 12 values between -1 and 1 -6 TANH activation function
 
-def value_function(state):
-    n_hidden1 = 400  
-    n_hidden2 = 400
-    n_outputs = 1
+### PROF THING ###
+# model = Sequential()
+# model.add(Conv2D(filters=6, kernel_size=(7, 7), strides=3, activation='relu', input_shape=(96, 96, self.frame_stack_num)))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Conv2D(filters=12, kernel_size=(4, 4), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Flatten())
+# model.add(Dense(216, activation='relu'))
+# model.add(Dense(len(self.action_space), activation=None))
+# model.compile(loss='mean_squared_error', optimizer=Adam(lr=self.learning_rate, epsilon=1e-7))
+
+
+def process_state_image(state):
+    state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+    state = state.astype(float)
+    state /= 255.0
+    return state
+
+
+def generate_state_frame_stack_from_queue(deque):
+    frame_stack = np.array(deque)
+    # Move stack dimension to the channel dimension (stack, x, y) -> (x, y, stack)
+    return np.transpose(frame_stack, (1, 2, 0))
+
+
+# Training
+def train_model(x_train,y_train,model):
+    RENDER                        = True
+    STARTING_EPISODE              = 1
+    ENDING_EPISODE                = 1000
+    SKIP_FRAMES                   = 2
+    TRAINING_BATCH_SIZE           = 64
+    SAVE_TRAINING_FREQUENCY       = 25
+    UPDATE_TARGET_MODEL_FREQUENCY = 5
     
-    with tf.variable_scope("value_network"):
-        hidden1 = layers.Dense(state, n_hidden1, tf.nn.elu)
-        hidden2 = layers.Dense(hidden1, n_hidden2, tf.nn.elu) 
-        V = layers.Dense(n_outputs, None)
-    return V
-
-
-def policy_network(state):
-    n_hidden1 = 40
-    n_hidden2 = 40
-    n_outputs = 1
+    agent = CarRacingDQNAgent(epsilon=0.3)
     
-    with tf.variable_scope("policy_network"):        
-        hidden1 = layers.Dense(state, n_hidden1, tf.nn.elu )
-        hidden2 = layers.Dense(hidden1, n_hidden2, tf.nn.elu)
-        mu = layers.Dense(hidden2, n_outputs, None,)
-        sigma = layers.Dense(hidden2, n_outputs, None)
-        sigma = tf.nn.softplus(sigma) + 1e-5
-        norm_dist = tf.contrib.distributions.Normal(mu, sigma)
-        action_tf_var = tf.squeeze(norm_dist.sample(1), axis=0)
-        action_tf_var = tf.clip_by_value(
-            action_tf_var, env.action_space.low[0], 
-            env.action_space.high[0])
-    return action_tf_var, norm_dist
+    for e in range(STARTING_EPISODE, ENDING_EPISODE+1):
+        init_state = ENV.reset()
+        init_state = process_state_image(init_state)
 
-################################################################
-#sample from state space for state normalization
-import sklearn
-import sklearn.preprocessingtf
-    [env.observation_space.sample() for x in range(10000)])
-scaler = sklearn.preprocessing.StandardScaler()
-scaler.fit(state_space_samples)
+        total_reward = 0
+        negative_reward_counter = 0
+        state_frame_stack_queue = deque([init_state]*agent.frame_stack_num, maxlen=agent.frame_stack_num)
+        time_frame_counter = 1
+        done = False
+        
+        while True:
+            if RENDER:
+                ENV.render()
 
-#function to normalize states
-def scale_state(state):                 #requires input shape=(2,)
-    scaled = scaler.transform([state])
-    return scaled                       #returns shape =(1,2)   
-###################################################################
-
-lr_actor = 0.00002  #set learning rates
-lr_critic = 0.001
-
-# define required placeholders
-action_placeholder = tf.placeholder(tf.float32)
-delta_placeholder = tf.placeholder(tf.float32)
-target_placeholder = tf.placeholder(tf.float32)
-
-action_tf_var, norm_dist = policy_network(state_placeholder)
-V = value_function(state_placeholder)
-
-# define actor (policy) loss function
-loss_actor = -tf.log(norm_dist.prob(action_placeholder) + 1e-5) * delta_placeholder
-training_op_actor = tf.train.AdamOptimizer(
-    lr_actor, name='actor_optimizer').minimize(loss_actor)
-
-# define critic (state-value) loss function
-loss_critic = tf.reduce_mean(tf.squared_difference(
-                             tf.squeeze(V), target_placeholder))
-training_op_critic = tf.train.AdamOptimizer(
-        lr_critic, name='critic_optimizer').minimize(loss_critic)
-################################################################
-#Training loop
-gamma = 0.99        #discount factor
-num_episodes = 300
-
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-episode_history = []
-for episode in range(num_episodes):
-    #receive initial state from E
-    state = env.reset()   # state.shape -> (2,)
-    reward_total = 0 
-    steps = 0
-    done = False
-    while (not done):
+            current_state_frame_stack = generate_state_frame_stack_from_queue(state_frame_stack_queue)
             
-        #Sample action according to current policy
-        #action.shape = (1,1)
-        action  = sess.run(action_tf_var, feed_dict={
-                        state_placeholder: scale_state(state)})
-        #Execute action and observe reward & next state from E
-        # next_state shape=(2,)    
-        #env.step() requires input shape = (1,)
-        next_state, reward, done, _ = env.step(
-                                np.squeeze(action, axis=0)) 
-        steps +=1
-        reward_total += reward
-        #V_of_next_state.shape=(1,1)
-        V_of_next_state = sess.run(V, feed_dict = 
-                {state_placeholder: scale_state(next_state)})  
-        #Set TD Target
-        #target = r + gamma * V(next_state)     
-        target = reward + gamma * np.squeeze(V_of_next_state) 
-        
-        # td_error = target - V(s)
-        #needed to feed delta_placeholder in actor training
-        td_error = target - np.squeeze(sess.run(V, feed_dict = 
-                    {state_placeholder: scale_state(state)})) 
-        
-        #Update actor by minimizing loss (Actor training)
-        _, loss_actor_val  = sess.run(
-            [training_op_actor, loss_actor], 
-            feed_dict={action_placeholder: np.squeeze(action), 
-            state_placeholder: scale_state(state), 
-            delta_placeholder: td_error})
-        #Update critic by minimizinf loss  (Critic training)
-        _, loss_critic_val  = sess.run(
-            [training_op_critic, loss_critic], 
-            feed_dict={state_placeholder: scale_state(state), 
-            target_placeholder: target})
-        
-        state = next_state
-        #end while
-    episode_history.append(reward_total)
-    print("Episode: {}, Number of Steps : {}, Cumulative reward: {:0.2f}".format(
-        episode, steps, reward_total))
+
+            # train_state = []
+            # train_target = []
+            # state = current_state_frame_stack
+            # # for state, action_index, reward, next_state, done in minibatch:
+            # target = model.predict(np.expand_dims(state, axis=0))[0]
+            # if done:
+            #     target[action_index] = reward
+            # else:
+            #     t = self.target_model.predict(np.expand_dims(next_state, axis=0))[0]
+            #     target[action_index] = reward + self.gamma * np.amax(t)
+            # train_state.append(state)
+            # train_target.append(target)
+            # self.model.fit(np.array(train_state), np.array(train_target), epochs=1, verbose=0)
+            # if self.epsilon > self.epsilon_min:
+            #     self.epsilon *= self.epsilon_decay
+
+            action = agent.act(current_state_frame_stack)
+
+            reward = 0
+            for _ in range(SKIP_FRAMES+1):
+                next_state, r, done, info = ENV.step(action)
+                reward += r
+                if done:
+                    break
+
+            # If continually getting negative reward 10 times after the tolerance steps, terminate this episode
+            negative_reward_counter = negative_reward_counter + 1 if time_frame_counter > 100 and reward < 0 else 0
+
+            # Extra bonus for the model if it uses full gas # Do that kind of shenanigans later
+            # if action[1] == 1 and action[2] == 0:
+            #     reward *= 1.5
+
+            total_reward += reward
+
+            next_state = process_state_image(next_state)
+            state_frame_stack_queue.append(next_state)
+            next_state_frame_stack = generate_state_frame_stack_from_queue(state_frame_stack_queue)
+
+            # agent.memorize(current_state_frame_stack, action, reward, next_state_frame_stack, done)
+
+            if done or negative_reward_counter >= 25 or total_reward < 0:
+                print('Episode: {}/{}, Scores(Time Frames): {}, Total Rewards(adjusted): {:.2}, Epsilon: {:.2}'.format(e, ENDING_EPISODE, time_frame_counter, float(total_reward), float(agent.epsilon)))
+                break
+
+
+def network():
+    '''OUR NETWORK'''
+    model = Sequential()
+    model.add(Conv2D(filters=16, kernel_size=(8,8), padding='same', data_format="channels_last", input_shape=(96, 96, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(filters=16, kernel_size=(3,3), padding='same', data_format="channels_last"))
+    model.add(BatchNormalization()) 
+    # model.add(Activation('relu'))
+
+    model.add(Flatten())
+
+    model.add(Dense(160,activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(160))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(len(ACTION_SPACE), activation='softmax'))
+
+    return model
+
+
+if __name__ == '__main__':
+    obs = ENV.reset()
+
+    model = network()
+    x_train,y_train=None,None
+    agent=None
     
-    if np.mean(episode_history[-100:]) > 90 and len(episode_history) >= 101:
-        print("****************Solved***************")
-        print("Mean cumulative reward over 100 episodes:{:0.2f}" .format(
-            np.mean(episode_history[-100:])))
+    train_model(x_train,y_train,model)
+    
+    dones = False
+    i = 0
+    # Play once trained !
+    while dones == False and i <= 10000:
+        print(f"[main_2.main] observation format {obs}")
+        print(f"[main_2.main] observation length {len(obs)}")        
+        q_value = model.predict(obs)
+        action = ACTION_SPACE[np.argmax(q_value)]
+        obs, rewards, dones, info = ENV.step(action)
+        ENV.render()
+        i+=1
+    print("there were " + str(i) + " steps")
