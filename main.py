1cdd7e8c10f04a6cc722654bed40dc7aafa0c1c9
Guillaume Grosse <45588739+guiguite38@users.noreply.github.com>
2021-01-27 17:23:02 +0100

    implemented Y model (q_value predictor) + training + play

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Concatenate
 from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import Input, Model
 
-from CarRacingDQNAgent import CarRacingDQNAgent
 import cv2
+from cv2 import cvtColor
 import numpy as np
 import gym
 from collections import deque
+import random
 
-# Environnement
+### Environnement ###
 ENV = gym.envs.make("CarRacing-v0")
 
-# Discretisation
-ACTION_SPACE    = [
-    (-1, 1, 0.2), (0, 1, 0.2), (1, 1, 0.2), # Plein gaz + 20% frein
+### Discretisation ###
+ACTION_SPACE = np.array((
+    (-1, 1, 0.2), (0, 1, 0.2), (1, 1, 0.2), # 3 directions (-1,0,1) + 100% gaz (1) + 20% frein (.2) 
     (-1, 1,   0), (0, 1,   0), (1, 1,   0), # Plein gaz + 0% frein
     (-1, 0, 0.2), (0, 0, 0.2), (1, 0, 0.2), # 0% gaz + 20% frein
     (-1, 0,   0), (0, 0,   0), (1, 0,   0)  # 0% gaz + 0% frein
-],
-
+))
 
 # input is 96*96*3 ints between 0 and 255
 # output must be 12 values between -1 and 1 -6 TANH activation function
-
-### PROF THING ###
-# model = Sequential()
-# model.add(Conv2D(filters=6, kernel_size=(7, 7), strides=3, activation='relu', input_shape=(96, 96, self.frame_stack_num)))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Conv2D(filters=12, kernel_size=(4, 4), activation='relu'))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(Flatten())
-# model.add(Dense(216, activation='relu'))
-# model.add(Dense(len(self.action_space), activation=None))
-# model.compile(loss='mean_squared_error', optimizer=Adam(lr=self.learning_rate, epsilon=1e-7))
-
 
 def process_state_image(state):
     state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
@@ -43,90 +33,42 @@ def process_state_image(state):
     return state
 
 
-def generate_state_frame_stack_from_queue(deque):
-    frame_stack = np.array(deque)
-    # Move stack dimension to the channel dimension (stack, x, y) -> (x, y, stack)
-    return np.transpose(frame_stack, (1, 2, 0))
-
-
-# Training
-def train_model(x_train,y_train,model):
-    RENDER                        = True
-    STARTING_EPISODE              = 1
-    ENDING_EPISODE                = 1000
-    SKIP_FRAMES                   = 2
-    TRAINING_BATCH_SIZE           = 64
-    SAVE_TRAINING_FREQUENCY       = 25
-    UPDATE_TARGET_MODEL_FREQUENCY = 5
-    
-    agent = CarRacingDQNAgent(epsilon=0.3)
-    
-    for e in range(STARTING_EPISODE, ENDING_EPISODE+1):
-        init_state = ENV.reset()
-        init_state = process_state_image(init_state)
-
-        total_reward = 0
-        negative_reward_counter = 0
-        state_frame_stack_queue = deque([init_state]*agent.frame_stack_num, maxlen=agent.frame_stack_num)
-        time_frame_counter = 1
-        done = False
-        
-        while True:
-            if RENDER:
-                ENV.render()
-
-            current_state_frame_stack = generate_state_frame_stack_from_queue(state_frame_stack_queue)
-            
-
-            # train_state = []
-            # train_target = []
-            # state = current_state_frame_stack
-            # # for state, action_index, reward, next_state, done in minibatch:
-            # target = model.predict(np.expand_dims(state, axis=0))[0]
-            # if done:
-            #     target[action_index] = reward
-            # else:
-            #     t = self.target_model.predict(np.expand_dims(next_state, axis=0))[0]
-            #     target[action_index] = reward + self.gamma * np.amax(t)
-            # train_state.append(state)
-            # train_target.append(target)
-            # self.model.fit(np.array(train_state), np.array(train_target), epochs=1, verbose=0)
-            # if self.epsilon > self.epsilon_min:
-            #     self.epsilon *= self.epsilon_decay
-
-            action = agent.act(current_state_frame_stack)
-
-            reward = 0
-            for _ in range(SKIP_FRAMES+1):
-                next_state, r, done, info = ENV.step(action)
-                reward += r
-                if done:
-                    break
-
-            # If continually getting negative reward 10 times after the tolerance steps, terminate this episode
-            negative_reward_counter = negative_reward_counter + 1 if time_frame_counter > 100 and reward < 0 else 0
-
-            # Extra bonus for the model if it uses full gas # Do that kind of shenanigans later
-            # if action[1] == 1 and action[2] == 0:
-            #     reward *= 1.5
-
-            total_reward += reward
-
-            next_state = process_state_image(next_state)
-            state_frame_stack_queue.append(next_state)
-            next_state_frame_stack = generate_state_frame_stack_from_queue(state_frame_stack_queue)
-
-            # agent.memorize(current_state_frame_stack, action, reward, next_state_frame_stack, done)
-
-            if done or negative_reward_counter >= 25 or total_reward < 0:
-                print('Episode: {}/{}, Scores(Time Frames): {}, Total Rewards(adjusted): {:.2}, Epsilon: {:.2}'.format(e, ENDING_EPISODE, time_frame_counter, float(total_reward), float(agent.epsilon)))
-                break
-
-
 def network():
+    """OUR NETWORK PREDICTING"""
+    # define two sets of inputs
+    input_state = Input(shape=(96,96,3))
+    input_action = Input(shape=(3,))
+
+    # the first branch operates on the first input = input_state
+    x = Conv2D(filters=16, kernel_size=(8,8), padding='same')(input_state)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Conv2D(filters=16, kernel_size=(3,3), padding='same')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = BatchNormalization()(x)
+    x = Flatten()(x)
+    x = Model(inputs=input_state, outputs=x)
+
+    # combine the output of the two branches
+    combined = Concatenate(axis=1)([x.output, input_action])
+
+    # apply a FC layer and then a regression prediction on the
+    # combined outputs
+    z = Dense(256, activation="relu")(combined)
+    # z = Dropout(0.5)(z)
+    z = Dense(128, activation="relu")(z)
+    z = Dense(12, activation="sigmoid")(z)
+
+    # our model will accept the inputs of the two branches and
+    # then output a single value
+    model = Model(inputs=[input_state, input_action], outputs=z)
+    return model
+
+
+def network2():
     '''OUR NETWORK'''
+    # ((96, 96, 3), (3, )) TODO : make this our input, can work with concat
     model = Sequential()
-    model.add(Conv2D(filters=16, kernel_size=(8,8), padding='same', data_format="channels_last", input_shape=(96, 96, 3)))
+    model.add(Conv2D(filters=16, kernel_size=(8,8), padding='same', data_format="channels_last", input_shape=(96, 96, 3))) # !! TODO : Change this to be [s,a]
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(filters=16, kernel_size=(3,3), padding='same', data_format="channels_last"))
@@ -150,18 +92,62 @@ def network():
     return model
 
 
-if __name__ == '__main__':
-    obs = ENV.reset()
+def generate_x_y(model, game, nb_episodes=1000, epsilon=0.3):
+    for i in range(nb_episodes):
+        s=game.reset()
+        x = []
+        y = []
+        cumul_rewards = 0
+        e = False
+        
+        while True:
+            # chose action with epsilon greedy
+            if np.random.random() < 0: #should be epsilon
+                a = random.choice(ACTION_SPACE)
+            else:
+                # ((96, 96, 3), (3, )) TODO : make this our input, can work with concat
 
-    model = network()
-    x_train,y_train=None,None
-    agent=None
+                # print(f"[main.generate_x_y] state {s}")
+                # print(f"[main.generate_x_y] actions {actions)}")
+                print(f"[main.generate_x_y] prediction {model.predict([s,ACTION_SPACE[0]])}")
+                # TODO CHECK INPUT DATA FOR MODEL PREDS
+                
+                idx = np.argmax([model.predict([s,a]) for a in ACTION_SPACE])
+                a = ACTION_SPACE[idx]
+            s1, r, e, _ = game.move(a)
+            # cumul_rewards += r
+            
+            # ================================= #
+            # Use DQN to estimate q_value below #
+            # ================================= #
+        
+            # save expected q_value ce sont les y
+            # save chosen action ce sont les x
+            x.append([s,a])
+
+            # Q[s,a] = Q[s,a] + alpha* (r + gamma * np.max(Q[s1,:]) - Q[s,a])
+            q_value = model.predict([s,a]) + alpha * (r + gamma * np.max(model.predict[s1,:]) - model.predict[s,a])
+            y.append(q_value)
+            s = s1
     
-    train_model(x_train,y_train,model)
+            if e == True:
+                break
+    return x,y
+
+
+if __name__ == '__main__':
+    model = network()
+    model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=1e-3), metrics=['accuracy', 'mse'])
+
+    training_cycles = 3
+    for i in range(training_cycles):
+        x,y = generate_x_y(model,ENV)
+        model.fit(x,y)
     
     dones = False
     i = 0
     # Play once trained !
+    obs = ENV.reset() 
     while dones == False and i <= 10000:
         print(f"[main_2.main] observation format {obs}")
         print(f"[main_2.main] observation length {len(obs)}")        
