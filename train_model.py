ac9cfbba19b3f299a74b20b3a589cdb4ca4deaad
Mahaut <mmahaut@ensc.fr>
2021-02-02 12:51:02 +0100

    4 frame observation, progressively longer epochs

--- train_model.py
+++ train_model.py
@@ -5,8 +5,8 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, MaxPooling2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import Input, Model
 
-import cv2
-from cv2 import cvtColor
+# import cv2
+# from cv2 import cvtColor
 import numpy as np
 import gym
 from collections import deque
@@ -18,12 +18,23 @@ ENV = gym.envs.make("CarRacing-v0")
 
 ### Discretisation ###
 # steer gas break in [-1,1] [0,1] [0,1] -> dtype float
-ACTION_SPACE = np.array([
-    [-1, 1, 0.2], [0, 1, 0.2], [1, 1, 0.2], # 3 directions (-1,0,1) + 100% gaz (1) + 20% frein (.2) 
-    [-1, 1,   0], [0, 1,   0], [1, 1,   0], # Plein gaz + 0% frein
-    [-1, 0, 0.2], [0, 0, 0.2], [1, 0, 0.2], # 0% gaz + 20% frein
-    [-1, 0,   0], [0, 0,   0], [1, 0,   0]  # 0% gaz + 0% frein
-], dtype=float)
+ACTION_SPACE = np.array(
+    [
+        [-1, 1, 0.2],
+        [0, 1, 0.2],
+        [1, 1, 0.2],  # 3 directions (-1,0,1) + 100% gaz (1) + 20% frein (.2)
+        [-1, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0],  # Plein gaz + 0% frein
+        [-1, 0, 0.2],
+        [0, 0, 0.2],
+        [1, 0, 0.2],  # 0% gaz + 20% frein
+        [-1, 0, 0],
+        [0, 0, 0],
+        [1, 0, 0],  # 0% gaz + 0% frein
+    ],
+    dtype=float,
+)
 
 # input is 96*96*3 ints between 0 and 255
 # output must be 12 values between -1 and 1 -6 TANH activation function
@@ -32,13 +43,13 @@ ACTION_SPACE = np.array([
 def network():
     """OUR NETWORK PREDICTING"""
     # define two sets of inputs
-    input_state = Input(shape=(96,96,3))
+    input_state = Input(shape=(96, 96, 12))
     input_action = Input(shape=(3,))
 
     # the first branch operates on the first input = input_state
-    x = Conv2D(filters=16, kernel_size=(8,8), padding='same')(input_state)
+    x = Conv2D(filters=16, kernel_size=(8, 8), padding="same")(input_state)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(filters=16, kernel_size=(3,3), padding='same')(x)
+    x = Conv2D(filters=16, kernel_size=(3, 3), padding="same")(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
     x = BatchNormalization()(x)
     x = Flatten()(x)
@@ -68,83 +79,108 @@ def process_state_image(state):
     # state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
     state = state.astype(float)
     state /= 255.0
-    state = state.reshape(96,96,3)
+    state = state.reshape(96, 96, 3)
     # print(f"[train_model.process_state_image] state type = {type(state)}")
-    # print(f"[train_model.process_state_image] state shape = {state.shape}")    
+    # print(f"[train_model.process_state_image] state shape = {state.shape}")
     return state
-    
 
-def generate_x_y(model, game, nb_episodes=10, epsilon=0.3, episode_time_limit=40):
-    print(f"[main.generate_x_y] entering generate_x_y, launching {nb_episodes} episodes")
+
+def generate_x_y(
+    model,
+    game,
+    nb_episodes=10,
+    epsilon=0.3,
+    episode_time_limit=40,
+    render=False,
+):
+    print(
+        f"[main.generate_x_y] entering generate_x_y, launching {nb_episodes} episodes"
+    )
     s_list = []
     a_list = []
     y = []
     for i in range(nb_episodes):
         print(f"--- episode {i} ---")
-        s=process_state_image(game.reset())
+        s = np.array([process_state_image(game.reset()) for _ in range(4)]).reshape(96,96,12)
         cumul_rewards = 0
         e = False
         start_time = time.time()
         nb_frames = 0
-        while e==False and time.time()-start_time <episode_time_limit:
-            nb_frames+=1
+        while e == False and time.time() - start_time < episode_time_limit:
+            nb_frames += 1
             # chose action with epsilon greedy
-            if np.random.random() < epsilon: #epsilon
+            if np.random.random() < epsilon:  # epsilon
                 a = random.choice(ACTION_SPACE)
+                q_value_predicts = []
+                idx = 0
+                q_value_predicts.append(model.predict([s.reshape(1,96,96,12), a.reshape(1,3)])[0])
             else:
                 states = np.array([s for _ in ACTION_SPACE])
-                idx = np.argmax(model.predict([states,ACTION_SPACE]))
+                q_value_predicts = model.predict([states, ACTION_SPACE])
+                idx = np.argmax(q_value_predicts)
                 a = ACTION_SPACE[idx]
 
             s1_unprocessed, r, e, _ = game.step(a)
-            
-            if a[1]==1 and a[2]==0:
-                r*=1.5
-            s1 = process_state_image(s1_unprocessed)
+
+            if a[1] == 1 and a[2] == 0:
+                r *= 1.5
+            s1 = np.concatenate((s[:,:,3:],process_state_image(s1_unprocessed)), axis=2)
 
             # cumul_rewards += r
             # save expected q_value ce sont les y
             # save chosen action ce sont les x
             s_list.append(s)
             a_list.append(a)
-            
-            s=s.reshape(1,96,96,3)
-            a=a.reshape(1,3)
 
-            q_value_predict = model.predict([s,a])
+            q_value_predict = q_value_predicts[idx]
             new_states = np.array([s1 for _ in ACTION_SPACE])
-            q_value_future = np.max([model.predict([new_states,ACTION_SPACE])])
+            q_value_future = np.max([model.predict([new_states, ACTION_SPACE])])
 
             # Q[s,a] = Q[s,a] + alpha* (r + gamma * np.max(Q[s1,:]) - Q[s,a])
-            q_value = q_value_predict + alpha * (r + gamma * q_value_future - q_value_predict)
-            
-            # print(f"[main.generate_x_y] q_value {q_value[0][0]}")
-            y.append(q_value[0][0])
+            q_value = q_value_predict + alpha * (
+                r + gamma * q_value_future - q_value_predict
+            )
+
+            print(f"[main.generate_x_y] q_value {q_value}\treward {r}")
+            y.append(q_value[0])
             s = s1
+
+            if render:
+                ENV.render()
         print(f"[main.generate_x_y] frames computed : {nb_frames}")
     return s_list, a_list, y
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     alpha = 0.1
     gamma = 0.9
     epsilon = 0.5
+    episode_time_limit=20
     model = network()
-    model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=1e-3), metrics=['mse'])
+    model.compile(
+        loss="mean_squared_error", optimizer=Adam(learning_rate=1e-3), metrics=["mse"]
+    )
 
     training_cycles = 10
 
     for cycle in range(training_cycles):
         print(f"Entering cycle {cycle}")
         epsilon = epsilon * 0.85
-        s, a, y = generate_x_y(model,ENV, epsilon=epsilon, nb_episodes=10)
-        s=np.array(s)
-        a=np.array(a)
-        y=np.array(y)
-        model.fit([s,a],y,verbose=2)
-    
+        episode_time_limit = min(episode_time_limit *1.1,50)
+        s, a, y = generate_x_y(
+            model,
+            ENV,
+            epsilon=epsilon,
+            nb_episodes=3,
+            episode_time_limit = episode_time_limit,
+            render=True,
+        )
+        s = np.array(s)
+        a = np.array(a)
+        y = np.array(y)
+        model.fit([s, a], y, verbose=2)
+
     print(f"[main.__main__] len y {len(y)}")
     print(f"[main.__main__] len states {len(s)}")
     print(f"[main.__main__] len actions {len(a)}")
-    model.save('models\\trained_model')
-    
+    model.save("models\\trained_model")
